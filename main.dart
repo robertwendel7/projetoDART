8e5310ebc993e2c4000fbe60963be73aa72e935b
Robert Wendel <134088034+robertwendel7@users.noreply.github.com>
2025-04-24 18:22:31 +0000

    primeiro commit

---
+++ main.dart
@@ -0,0 +1,16 @@
+void main(){
+  var idade = 28;
+  var num1 = 10;
+  var num2 = 3;
+  double altura = 1.75;
+  bool lampadaLigada = false;
+  String nomeAnimal = "Frank";
+  
+
+  print(idade);
+  print(altura);
+  print(nomeAnimal);
+  print(lampadaLigada);
+  print(num1 + num2);
+  print(num1 % num2);
+}
